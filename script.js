55be2a87f78a383cf3ac4eb90317e03dc97495e1
sohaibdoma <sohaibdoma78@gmail.com>
2026-02-02 20:28:57 +0300

    Make menu item clicks behave like close button

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ const menuButton = document.querySelector('.menu-button');
 const menuOverlay = document.querySelector('.menu-overlay');
 const menuLinks = document.querySelectorAll('.menu-overlay a');
 
-menuButton.addEventListener('click', () => {
+// Toggle menu (≡ ↔ X)
+function toggleMenu() {
   menuButton.classList.toggle('open');
   menuOverlay.classList.toggle('open');
-});
+}
 
-// Close menu when a menu link is clicked
+// Close menu (force close)
+function closeMenu() {
+  menuButton.classList.remove('open');
+  menuOverlay.classList.remove('open');
+}
+
+// Menu button click
+menuButton.addEventListener('click', toggleMenu);
+
+// Menu item clicks behave EXACTLY like clicking X
 menuLinks.forEach(link => {
   link.addEventListener('click', () => {
-    menuButton.classList.remove('open');
-    menuOverlay.classList.remove('open');
+    closeMenu();
   });
 });
