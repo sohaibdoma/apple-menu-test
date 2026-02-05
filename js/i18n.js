565ecd86bb2d5094b42a220c4f377a7fe7169750
sohaibdoma <sohaibdoma78@gmail.com>
2026-02-05 16:41:27 +0300

    Add language loading feature in i18n.js
    Implement language loading functionality from JSON files.

---
+++ js/i18n.js
@@ -0,0 +1,18 @@
+const DEFAULT_LANG = 'ar';
+
+function loadLanguage(lang) {
+  fetch(`lang/${lang}.json`)
+    .then(res => res.json())
+    .then(data => {
+      document.documentElement.lang = data.lang;
+      document.documentElement.dir = data.dir;
+
+      document.querySelectorAll('[data-i18n]').forEach(el => {
+        const key = el.dataset.i18n;
+        if (data[key]) el.textContent = data[key];
+      });
+
+      localStorage.setItem('lang', lang);
+      updateActiveButton(lang);
+    });
+}
