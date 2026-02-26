b0da334687828cd22d4cbdd22a15094bd0f4439e
sohaibdoma <sohaibdoma78@gmail.com>
2026-02-26 15:27:16 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -22,9 +22,31 @@ window.Wahyollah = window.Wahyollah || {};
     return fetchJson(`${API_BASE}/chapters/${id}?language=en`);
   }
 
-  async function getUthmaniVersesByChapter(id) {
-    return fetchJson(`${API_BASE}/quran/verses/uthmani?chapter_number=${id}`);
+ async function getUthmaniVersesByChapter(chapterNumber) {
+  const perPage = 50;
+  let page = 1;
+  let verses = [];
+
+  while (true) {
+    const url =
+      `${API_BASE}/verses/by_chapter/${chapterNumber}` +
+      `?fields=text_uthmani,page_number,verse_number,verse_key` +
+      `&per_page=${perPage}&page=${page}`;
+
+    const data = await fetchJson(url);
+
+    if (data?.verses?.length) {
+      verses = verses.concat(data.verses);
+    }
+
+    const next = data?.pagination?.next_page;
+    if (!next) break;
+
+    page = next;
   }
+
+  return { verses };
+}
 
   window.Wahyollah.api = {
     getChapter,
