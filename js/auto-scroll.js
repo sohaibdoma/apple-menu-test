0255e36eab16efbc71d3dffb9cd6bc016817d8be
sohaibdoma <sohaibdoma78@gmail.com>
2026-03-03 14:05:21 +0300

    Update auto-scroll.js

--- js/auto-scroll.js
+++ js/auto-scroll.js
@@ -16,7 +16,7 @@
     let lastT = 0;
 
     // Adjust this single number if you want different speed
-    const SPEED = prefersReduced ? 0 : 55;
+    const SPEED = prefersReduced ? 0 : 25;
 
     /* ===============================
        UI State (CSS handles icon easing)
