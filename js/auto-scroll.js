3f6136a5588813e4ad7f0ec6993b3d732a4e5d1b
sohaibdoma <sohaibdoma78@gmail.com>
2026-03-06 10:17:31 +0300

    Update auto-scroll.js

--- js/auto-scroll.js
+++ js/auto-scroll.js
@@ -25,7 +25,7 @@
     let programmaticScroll = false;
 
     // Set speed here (7 / 10 / 15 etc.)
-    const SPEED = prefersReduced ? 0 : 1;
+    const SPEED = prefersReduced ? 0 : 0.5;
 
     // =========================
     // WAKE LOCK (Stop screen timeout)
