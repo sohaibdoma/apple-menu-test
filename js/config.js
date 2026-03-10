f1f58386cc45de97cf886f515e312bb94e0b813b
sohaibdoma <sohaibdoma78@gmail.com>
2026-03-10 14:57:03 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,7 @@
 window.Wahyollah = window.Wahyollah || {};
 
 window.Wahyollah.config = {
-  defaultLang: "ar",
-  supportedLangs: ["ar", "en", "tr"],
-
-  // Used by i18n loader
-  langPath: (lang) => `lang/${lang}.json`,
-
-  // Used by menu loader
   menuPath: "components/menu.html",
+  defaultQuranMode: "tajweed",
+  supportedQuranModes: ["reading", "tajweed", "mushaf"]
 };
