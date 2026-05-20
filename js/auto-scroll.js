203fd5a5ef24270f884ee4a29e35c8102f00c506
sohaibdoma <sohaibdoma78@gmail.com>
2026-05-20 13:08:22 +0300

    Decrease manual scroll grace period
    Reduced manual scroll grace period from 1200ms to 120ms.

--- js/auto-scroll.js
+++ js/auto-scroll.js
@@ -27,7 +27,7 @@
 
     const SPEED_PX_PER_SECOND = prefersReduced ? 0 : 18;
     const BOTTOM_THRESHOLD = 3;
-    const MANUAL_SCROLL_GRACE_MS = 1200;
+    const MANUAL_SCROLL_GRACE_MS = 120;
 
     function getMaxScrollTop() {
       return Math.max(0, scroller.scrollHeight - window.innerHeight);
